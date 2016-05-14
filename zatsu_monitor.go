84991e505053e52f24ac31462875559ef64d37f1
sue445 <sue445@sue445.net>
2016-05-15 00:37:07 +0900

    Impl GetDbStatus

--- zatsu_monitor.go
+++ zatsu_monitor.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"bytes"
+	"github.com/syndtr/goleveldb/leveldb"
 	"net/http"
 )
 
 var OK_STATUS_CODES = []int{200, 301, 302, 303, 304, 307, 308}
 
+const (
+	STATUS_TRUE  = "1"
+	STATUS_FALSE = "0"
+)
+
 type ZatsuMonitor struct {
+	databaseFile string
+}
+
+func NewZatsuMonitor(databaseFile string) *ZatsuMonitor {
+	z := new(ZatsuMonitor)
+	z.databaseFile = databaseFile
+	return z
 }
 
 func (z ZatsuMonitor) CheckUrl(url string) bool {
@@ -23,4 +37,31 @@ func (z ZatsuMonitor) CheckUrl(url string) bool {
 	}
 
 	return false
+}
+
+func (z ZatsuMonitor) GetDbStatus(key string) bool {
+	db, err := leveldb.OpenFile(z.databaseFile, nil)
+	if err != nil {
+		panic("Failed: OpenFile " + z.databaseFile)
+	}
+	defer db.Close()
+
+	if ret, _ := db.Has([]byte(key), nil); !ret {
+		// If not exists key, same as success (1st operation)
+		return true
+	}
+
+	data, err := db.Get([]byte(key), nil)
+
+	buf := bytes.NewBuffer(data)
+	value := buf.String()
+
+	switch value {
+	case STATUS_TRUE:
+		return true
+	case STATUS_FALSE:
+		return false
+	default:
+		panic("Unknown value: " + value)
+	}
 }
--- zatsu_monitor_test.go
+++ zatsu_monitor_test.go
@@ -2,17 +2,68 @@ package main
 
 import (
 	"github.com/stretchr/testify/assert"
+	"github.com/syndtr/goleveldb/leveldb"
 	"testing"
 )
 
+const TEST_DB_FILE = "tmp/zatsu"
+
+func NewTestZatsuMonitor() *ZatsuMonitor {
+	return NewZatsuMonitor(TEST_DB_FILE)
+}
+
 func TestZatsuMonitor_CheckUrl_Ok(t *testing.T) {
-	z := new(ZatsuMonitor)
+	z := NewTestZatsuMonitor()
 	actual := z.CheckUrl("https://www.google.co.jp/")
 	assert.Equal(t, true, actual)
 }
 
 func TestZatsuMonitor_CheckUrl_Ng(t *testing.T) {
-	z := new(ZatsuMonitor)
+	z := NewTestZatsuMonitor()
 	actual := z.CheckUrl("https://www.google.co.jp/aaa")
 	assert.Equal(t, false, actual)
+}
+
+func PutData(key string, value string) {
+	db, err := leveldb.OpenFile(TEST_DB_FILE, nil)
+	if err != nil {
+		panic("Failed: OpenFile " + TEST_DB_FILE)
+	}
+	defer db.Close()
+
+	db.Put([]byte(key), []byte(value), nil)
+}
+
+func DeleteData(key string) {
+	db, err := leveldb.OpenFile(TEST_DB_FILE, nil)
+	if err != nil {
+		panic("Failed: OpenFile " + TEST_DB_FILE)
+	}
+	defer db.Close()
+
+	db.Delete([]byte(key), nil)
+}
+
+func TestZatsuMonitor_GetDbStatus_ExistsTrue(t *testing.T) {
+	PutData("key", STATUS_TRUE)
+	defer DeleteData("key")
+
+	z := NewTestZatsuMonitor()
+	actual := z.GetDbStatus("key")
+	assert.Equal(t, true, actual)
+}
+
+func TestZatsuMonitor_GetDbStatus_ExistsFalse(t *testing.T) {
+	PutData("key", STATUS_FALSE)
+	defer DeleteData("key")
+
+	z := NewTestZatsuMonitor()
+	actual := z.GetDbStatus("key")
+	assert.Equal(t, false, actual)
+}
+
+func TestZatsuMonitor_GetDbStatus_NotExists(t *testing.T) {
+	z := NewTestZatsuMonitor()
+	actual := z.GetDbStatus("key")
+	assert.Equal(t, true, actual)
 }
