84aaeb7c111fd6a18e7e588575716f8d9a245002
sue445 <sue445@sue445.net>
2016-05-17 01:49:54 +0900

    Add test

--- config_test.go
+++ config_test.go
@@ -34,6 +34,34 @@ name2:
 	assert.Equal(t, "111111", config["name2"]["room_id"])
 }
 
+func TestLoadConfigFromData2(t *testing.T) {
+	yamlData := `name1: &common
+  check_url: "http://example.com/1"
+  type: slack
+  webhook_url: "https://hooks.slack.com/services/AAAAAAAAA/BBBBBBBB/CCCCCCCCCCCCC"
+  user_name: "zatsu_monitor"
+  channel: "#general"
+name2:
+  <<: *common
+  channel: "#random"`
+
+	config, err := LoadConfigFromData(yamlData)
+
+	assert.NoError(t, err)
+
+	assert.Equal(t, "http://example.com/1", config["name1"]["check_url"])
+	assert.Equal(t, "slack", config["name1"]["type"])
+	assert.Equal(t, "https://hooks.slack.com/services/AAAAAAAAA/BBBBBBBB/CCCCCCCCCCCCC", config["name1"]["webhook_url"])
+	assert.Equal(t, "zatsu_monitor", config["name1"]["user_name"])
+	assert.Equal(t, "#general", config["name1"]["channel"])
+
+	assert.Equal(t, "http://example.com/1", config["name2"]["check_url"])
+	assert.Equal(t, "slack", config["name2"]["type"])
+	assert.Equal(t, "https://hooks.slack.com/services/AAAAAAAAA/BBBBBBBB/CCCCCCCCCCCCC", config["name2"]["webhook_url"])
+	assert.Equal(t, "zatsu_monitor", config["name2"]["user_name"])
+	assert.Equal(t, "#random", config["name2"]["channel"])
+}
+
 func TestLoadConfigFromFile(t *testing.T) {
 	config, err := LoadConfigFromFile("test/config.yml")
 
