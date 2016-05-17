1221d9eb77f04d348f29517dd19a9a18e4581599
sue445 <sue445@sue445.net>
2016-05-17 23:51:47 +0900

    Impl args

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+)
 
+func main() {
+	configFile := *flag.String("config", "", "Path to config file")
+	dataDir := *flag.String("data", "", "Path to data dir for cache")
+	configName := *flag.String("name", "", "Name for url checking (default: all)")
+	flag.Parse()
+
+	fmt.Printf("configFile=%v\n", configFile)
+	fmt.Printf("dataDir=%v\n", dataDir)
+	fmt.Printf("configName=%v\n", configName)
 }
