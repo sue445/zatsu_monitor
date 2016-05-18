7b9d7356f9d05ee436fe9327e3c5354c52a07037
sue445 <sue445@sue445.net>
2016-05-18 20:44:26 +0900

    Impl slack notifier

---
+++ slack_notifier.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nlopes/slack"
+)
+
+var slackExpectedKeys = []string{"type", "check_url", "webhook_url", "user_name", "channel"}
+
+type SlackNotifier struct {
+	token    string
+	userName string
+	channel  string
+}
+
+func NewSlackNotifier(token string, userName string, channel string) *SlackNotifier {
+	s := new(SlackNotifier)
+	s.token = token
+	s.userName = userName
+	s.channel = channel
+	return s
+}
+
+func (s SlackNotifier) ExpectedKeys() []string {
+	return slackExpectedKeys
+}
+
+func (s SlackNotifier) PostStatus(checkUrl string, beforeStatusCode int, currentStatusCode int) error {
+	var statusText, iconEmoji, userName string
+
+	successful := IsSuccessfulStatus(currentStatusCode)
+
+	if successful {
+		statusText = "ok"
+		iconEmoji = ":green_heart:"
+		userName = s.userName + " Successful"
+	} else {
+		statusText = "down"
+		iconEmoji = ":broken_heart:"
+		userName = s.userName + " Failure"
+	}
+
+	format := `%s is %s
+statusCode: %d -> %d`
+	message := fmt.Sprintf(format, checkUrl, statusText, beforeStatusCode, currentStatusCode)
+
+	params := slack.NewPostMessageParameters()
+	params.Username = userName
+	params.IconEmoji = iconEmoji
+
+	api := slack.New(s.token)
+
+	_, _, err := api.PostMessage(s.channel, message, params)
+
+	return err
+}
