4339e8877313fe239f9cb80c6081ffeccc3dfa61
sue445 <sue445@sue445.net>
2016-05-18 20:53:27 +0900

    Refactor: chatwork_notifier

--- chatwork_notifier_test.go
+++ chatwork_notifier_test.go
@@ -7,28 +7,34 @@ import (
 	"testing"
 )
 
-var notifier *ChatworkNotifier
-
-func TestMain(m *testing.M) {
-	err := godotenv.Load()
-	if err != nil {
-		// If not found .env, skip test
-		return
-	}
+func NewTestChatworkNotifier() *ChatworkNotifier {
+	godotenv.Load()
 
 	apiToken := os.Getenv("CHATWORK_API_TOKEN")
 	roomId := os.Getenv("CHATWORK_ROOM_ID")
-	notifier = NewChatworkNotifier(apiToken, roomId)
 
-	os.Exit(m.Run())
+	if len(apiToken) == 0 || len(roomId) == 0 {
+		return nil
+	}
+	return NewChatworkNotifier(apiToken, roomId)
 }
 
 func TestChatworkNotifier_PostStatus_True(t *testing.T) {
+	notifier := NewTestChatworkNotifier()
+	if notifier == nil {
+		return
+	}
+
 	err := notifier.PostStatus("https://www.google.co.jp/", 0, 200)
 	assert.NoError(t, err)
 }
 
 func TestChatworkNotifier_PostStatus_False(t *testing.T) {
+	notifier := NewTestChatworkNotifier()
+	if notifier == nil {
+		return
+	}
+
 	err := notifier.PostStatus("https://www.google.co.jp/aaa", 0, 404)
 	assert.NoError(t, err)
 }
