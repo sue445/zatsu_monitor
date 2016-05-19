90cc6a1678316e9219aa72fcbd4ee6701f264ee2
sue445 <sue445@sue445.net>
2016-05-20 01:05:40 +0900

    Refactor: token -> api_token

--- config_test.go
+++ config_test.go
@@ -9,7 +9,7 @@ func TestLoadConfigFromData(t *testing.T) {
 	yamlData := `name1:
   check_url: "http://example.com/1"
   type: slack
-  webhook_url: "https://hooks.slack.com/services/AAAAAAAAA/BBBBBBBB/CCCCCCCCCCCCC"
+  api_token: "xoxp-0000000000-0000000000-0000000000-000000"
   user_name: "zatsu_monitor"
   channel: "#general"
 name2:
@@ -24,7 +24,7 @@ name2:
 
 	assert.Equal(t, "http://example.com/1", config["name1"]["check_url"])
 	assert.Equal(t, "slack", config["name1"]["type"])
-	assert.Equal(t, "https://hooks.slack.com/services/AAAAAAAAA/BBBBBBBB/CCCCCCCCCCCCC", config["name1"]["webhook_url"])
+	assert.Equal(t, "xoxp-0000000000-0000000000-0000000000-000000", config["name1"]["api_token"])
 	assert.Equal(t, "zatsu_monitor", config["name1"]["user_name"])
 	assert.Equal(t, "#general", config["name1"]["channel"])
 
@@ -38,7 +38,7 @@ func TestLoadConfigFromData2(t *testing.T) {
 	yamlData := `name1: &common
   check_url: "http://example.com/1"
   type: slack
-  webhook_url: "https://hooks.slack.com/services/AAAAAAAAA/BBBBBBBB/CCCCCCCCCCCCC"
+  api_token: "xoxp-0000000000-0000000000-0000000000-000000"
   user_name: "zatsu_monitor"
   channel: "#general"
 name2:
@@ -51,13 +51,13 @@ name2:
 
 	assert.Equal(t, "http://example.com/1", config["name1"]["check_url"])
 	assert.Equal(t, "slack", config["name1"]["type"])
-	assert.Equal(t, "https://hooks.slack.com/services/AAAAAAAAA/BBBBBBBB/CCCCCCCCCCCCC", config["name1"]["webhook_url"])
+	assert.Equal(t, "xoxp-0000000000-0000000000-0000000000-000000", config["name1"]["api_token"])
 	assert.Equal(t, "zatsu_monitor", config["name1"]["user_name"])
 	assert.Equal(t, "#general", config["name1"]["channel"])
 
 	assert.Equal(t, "http://example.com/1", config["name2"]["check_url"])
 	assert.Equal(t, "slack", config["name2"]["type"])
-	assert.Equal(t, "https://hooks.slack.com/services/AAAAAAAAA/BBBBBBBB/CCCCCCCCCCCCC", config["name2"]["webhook_url"])
+	assert.Equal(t, "xoxp-0000000000-0000000000-0000000000-000000", config["name2"]["api_token"])
 	assert.Equal(t, "zatsu_monitor", config["name2"]["user_name"])
 	assert.Equal(t, "#random", config["name2"]["channel"])
 }
@@ -69,7 +69,7 @@ func TestLoadConfigFromFile(t *testing.T) {
 
 	assert.Equal(t, "http://example.com/1", config["name1"]["check_url"])
 	assert.Equal(t, "slack", config["name1"]["type"])
-	assert.Equal(t, "https://hooks.slack.com/services/AAAAAAAAA/BBBBBBBB/CCCCCCCCCCCCC", config["name1"]["webhook_url"])
+	assert.Equal(t, "xoxp-0000000000-0000000000-0000000000-000000", config["name1"]["api_token"])
 	assert.Equal(t, "zatsu_monitor", config["name1"]["user_name"])
 	assert.Equal(t, "#general", config["name1"]["channel"])
 
