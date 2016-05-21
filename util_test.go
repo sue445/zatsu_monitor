2dcda2ea0243b087545df0b7cf1b82b939023f5d
sue445 <sue445@sue445.net>
2016-05-21 12:48:21 +0900

    Add test

--- util_test.go
+++ util_test.go
@@ -12,11 +12,18 @@ func TestUtil_HttpStatusCode_Ok(t *testing.T) {
 	assert.Equal(t, 200, actual)
 }
 
-func TestUtil_HttpStatusCodeg_Ng(t *testing.T) {
+func TestUtil_HttpStatusCodeg_HttpError(t *testing.T) {
 	actual, err := HttpStatusCode("https://www.google.co.jp/aaa")
 
 	assert.NoError(t, err)
 	assert.Equal(t, 404, actual)
+}
+
+func TestUtil_HttpStatusCodeg_NoSuchHost(t *testing.T) {
+	actual, err := HttpStatusCode("https://aaaaaaaaaaaaaaa")
+
+	assert.Error(t, err)
+	assert.Equal(t, 0, actual)
 }
 
 func TestUtil_IsSuccessful(t *testing.T) {
