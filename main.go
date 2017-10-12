b4ef7159c2dfeb12279b2d76db51eabdfaa81419
sue445 <sue445@sue445.net>
2017-10-13 00:01:36 +0900

    [Resolved] don't use underscores in Go names; var notifier_type should be notifierType

--- main.go
+++ main.go
@@ -51,17 +51,17 @@ func printVersion() {
 }
 
 func perform(name string, values map[string]string) {
-	notifier_type := values["type"]
+	notifierType := values["type"]
 
 	var notifier Notifier
 
-	switch notifier_type {
+	switch notifierType {
 	case "chatwork":
 		notifier = NewChatworkNotifier(values["api_token"], values["room_id"])
 	case "slack":
 		notifier = NewSlackNotifier(values["api_token"], values["user_name"], values["channel"])
 	default:
-		panic(fmt.Sprintf("Unknown type: %s in %s", notifier_type, configFile))
+		panic(fmt.Sprintf("Unknown type: %s in %s", notifierType, configFile))
 	}
 
 	// If it does not exist even one expected key, skip
